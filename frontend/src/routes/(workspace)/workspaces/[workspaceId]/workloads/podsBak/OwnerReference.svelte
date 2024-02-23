3dda0baa13376d12f9c3d56ae0babd3d54306882
Firdaus <mail4frenz@gmail.com>
2024-02-24 05:32:31 +0800

    replace tanstack table with svelte-headless-table

---
+++ frontend/src/routes/(workspace)/workspaces/[workspaceId]/workloads/podsBak/OwnerReference.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let data:any;
+</script>
+
+<div class="flex flex-col w-[200px] gap-1">
+    {#if data}
+        {#each data as d (d.uid)}
+            <div class="">
+                <p>{d.kind}</p>
+            </div>
+        {/each}
+    {/if}
+
+</div>
